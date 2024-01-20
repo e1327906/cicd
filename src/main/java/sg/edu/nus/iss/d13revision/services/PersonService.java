6594e646a765013062510e3a5a015b2c6dbf6e30
Zaw <min_than_zaw@msi-global.com.sg>
2024-01-20 12:51:11 +0800

    clean up

---
+++ src/main/java/sg/edu/nus/iss/d13revision/services/PersonService.java
@@ -0,0 +1,43 @@
+package sg.edu.nus.iss.d13revision.services;
+
+import org.springframework.stereotype.Service;
+import sg.edu.nus.iss.d13revision.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public PersonService() {
+        persons.add(new Person("Mark", "Zuckerberg"));
+        persons.add(new Person("Elon", "Musk"));
+    }
+
+    public List<Person> getPersons() {
+        return this.persons;
+    }
+
+    public void addPerson(Person p) {
+        persons.add(new Person(p.getFirstName(), p.getLastName()));
+    }
+
+    public void updatePerson(Person p) {
+        Person foundPerson = persons.stream().filter(o -> o.getId().equals(p.getId())).findAny().orElse(null);
+
+        Person updatedPerson = new Person();
+        updatedPerson.setId(p.getId());
+        updatedPerson.setFirstName(p.getFirstName());
+        updatedPerson.setLastName(p.getLastName());
+
+        persons.remove(foundPerson);
+        persons.add(new Person(updatedPerson.getId(), updatedPerson.getFirstName(), updatedPerson.getLastName()));
+    }
+
+    public void removePerson(Person p) {
+        Person foundPerson = persons.stream().filter(o -> o.getId().equals(p.getId())).findAny().orElse(null);
+
+        persons.remove(foundPerson);
+    }
+}
