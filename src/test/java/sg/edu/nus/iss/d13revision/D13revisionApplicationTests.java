b0eccb4e906b4a215c10832a522de86b928dcaf8
Zaw <min_than_zaw@msi-global.com.sg>
2024-01-20 13:04:36 +0800

    clean up

--- src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
@@ -1,13 +1,19 @@
-package d13revision;
+package sg.edu.nus.iss.d13revision;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class D13revisionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("ExpectedResult", "ExpectedResult");
 	}
+
+
+
 
 }
